2d3834bfdc499fb76645848d25ccff123d210a0b
pinkiwinkie <elrumor99@gmail.com>
2023-01-09 20:23:28 +0100

    Coordinate

--- src/main/java/model/Coordinate.java
+++ src/main/java/model/Coordinate.java
@@ -1,4 +1,56 @@
 package model;
 
 public class Coordinate {
+    private int number;
+    private char letter;
+
+    public Coordinate(char letter, int number) {
+        this.number = number;
+        this.letter = letter;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public char getLetter() {
+        return letter;
+    }
+
+    public Coordinate up() {
+        return new Coordinate(letter, number - 1);
+    }
+
+    public Coordinate down() {
+        return new Coordinate(letter, number + 1);
+    }
+
+    public Coordinate right() {
+        return new Coordinate((char) (letter + 1), number);
+    }
+
+    public Coordinate left() {
+        return new Coordinate((char) (letter - 1), number);
+    }
+
+    public Coordinate diagonalUpLeft() {
+        return up().left();
+    }
+
+    public Coordinate diagonalUpRight() {
+        return up().right();
+    }
+
+    public Coordinate diagonalDownLeft() {
+        return down().left();
+    }
+
+    public Coordinate diagonalDownRight() {
+        return down().right();
+    }
+
+    @Override
+    public String toString() {
+        return "("+ letter + "," + number + " )";
+    }
 }
