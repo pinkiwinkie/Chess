82f3dcf4d9e574ea73b1b334dd2b308c7a5c5287
pinkiwinkie <97902221+pinkiwinkie@users.noreply.github.com>
2023-01-24 17:38:43 +0100

    Tablero

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ import model.Piece;
 
 public class Main {
     public static void main(String[] args) {
+        Board board = new Board();
+        System.out.println(board);
         Coordinate c1 = new Coordinate('c', 1);
         Piece whiteKnight = new Piece(Piece.Type.WHITE_KNIGHT, new Cell(new Board(), c1));
         System.out.println(whiteKnight);
