f9f612526504f411faee6ce4095322d2ced90348
pinkiwinkie <97902221+pinkiwinkie@users.noreply.github.com>
2023-03-12 21:44:44 +0100

    ready to push 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,6 @@ import tools.View;
 
 public class Main {
     public static void main(String[] args) {
-//        Board board = new Board();
-//        board.placePieces();
-//        board.highlight(board.getCell(new Coordinate('D',8)).getPiece().getNextMovements());
-//        View.show(board, Piece.Color.BLACK);
-//        View.show(board, Piece.Color.WHITE);
         Menu.menu();
     }
 }
